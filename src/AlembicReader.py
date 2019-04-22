e45540adf0d3be028a588b27d6c68656b3102a97
Yusuke Kawauchi <ykawauchi-tuk@g.ecc.u-tokyo.ac.jp>
2019-04-22 18:07:44 +0900

    [fix]

---
+++ src/AlembicReader.py
@@ -0,0 +1,40 @@
+import hdf5plugin
+import h5py
+import numpy as np
+
+class AlembicReader():
+    def __init__(self,input_dir,time_step):
+        self.input_dir = input_dir
+        self.time_step = time_step
+
+    def alembic_reader(self):
+        errors = []
+        points = []
+        _min_curve, _max_curve = self.get_curve_count()
+        with h5py.File(self.input_dir,'r') as f:
+            for p in [f'ABC/hairSystem1OutputCurves/curve{i}/curveShape{i}/.prop/.geom/P.smpi/{self.time_step:04}' for i in range(_min_curve,_max_curve)]:
+                try:
+                    points.append(f[p].value)        
+                except KeyError as e:
+                    errors.append(e)
+            if errors == []:
+                return points, "ok"
+            else:
+                return [], errors
+
+    def get_curve_count(self):
+        key = []
+        with h5py.File(self.input_dir,'r') as f:
+            for k in f['ABC/hairSystem1OutputCurves'].keys():
+                if k == '.prop':
+                    pass
+                else:
+                    key.append(int(k.strip('curve')))
+        return min(key), max(key)
+
+def main():
+    ar = AlembicReader('../data/sample_hdf5.abc',1)
+    print(ar.alembic_reader())
+
+if __name__ == "__main__":
+    main()
---
+++ src/SplineToPoints.py
@@ -0,0 +1,50 @@
+# for test
+import hdf5plugin
+import h5py
+# 3次のBSpline関数
+from scipy.interpolate import splprep,splev
+import numpy as np
+# 描画関数(draw)用に定義
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+class SplineToPoints():
+    # 入力は3*10の制御点をndarray化したものをspline数だけリスト化したlist object.
+    def __init__(self,li,points=50):
+        self.points_positions = li
+        self.points = points
+        self.sp_points = []
+        
+    # 入力の制御点から，3次のBSpline曲線を構築し，その上の50点をpickupする関数．
+    def spline_to_points(self):
+        for arr in self.points_positions:
+            point = np.reshape(arr,(3,10),order='F')
+            tck,u = splprep(point,k=3)
+            params = np.linspace(u[0], u[-1], num=self.points, endpoint=True)
+            # 出力は，values[0] -> x, values[1] -> y, values[2] -> zが格納されている．
+            values = splev(params, tck)
+            self.sp_points.append(values)
+        return self.sp_points
+    
+    # 表示用の関数
+    def draw(self, num=0):
+        fig = plt.figure()
+        ax = fig.gca(projection='3d')
+        ax.scatter(xs=self.sp_points[num][0], ys=self.sp_points[num][1], zs=self.sp_points[num][2],
+                color='deeppink', label='cubic spline')
+        ax.set_xlabel('x')
+        ax.set_ylabel('y')
+        ax.set_zlabel('z')
+        ax.legend()
+        plt.show()
+    
+def main():
+    arr = []
+    with h5py.File('../data/sample_hdf5.abc','r') as f:
+        arr.append(f['ABC/hairSystem1OutputCurves/curve270/curveShape270/.prop/.geom/P.smpi/0030'].value)
+    sp = SplineToPoints(arr)
+    sp.spline_to_points()
+    sp.draw()
+
+if __name__ == '__main__':
+    main()
